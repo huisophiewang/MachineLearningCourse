82e513ae40888833a806eda817e09ce1d05a26cc
Sophie Wang <sophie@Sophies-MacBook.local>
2016-10-03 23:47:42 -0400

    work on hw1 ridge reg

--- linear_regression.py
+++ linear_regression.py
@@ -13,18 +13,65 @@ def poly_basis_trans(X, degree):
     return X_trans
     
 
-def linear_reg():
-    data = scipy.io.loadmat('linear_regression.mat')
-    #print data
-    x_train, y_train = data['X_trn'], data['Y_trn']
-    x_test, y_test = data['X_tst'], data['Y_tst']
+def linear_reg(data):
+    degree_cases = [2, 5, 10, 20]
+    for degree in degree_cases:
+        # reset x_train, x_test, since poly_basis_trans overwrites it
+        x_train, y_train = data['X_trn'], data['Y_trn']
+        x_test, y_test = data['X_tst'], data['Y_tst']
+        print '----------------------'
+        print "degree=%d" % degree
+        x_train = poly_basis_trans(x_train, degree)
+        pseudo_inv = np.linalg.inv(np.dot(np.transpose(x_train), x_train))
+        pseduo_inv = np.dot(pseudo_inv, np.transpose(x_train))
+        w = np.dot(pseduo_inv, y_train)
+        print "Cofficients w: %s" % np.transpose(w)   
+        train_err = np.mean((np.dot(x_train, w) - y_train) ** 2)
+        print "Training error: %s" % train_err
+        x_test = poly_basis_trans(x_test, degree)
+        test_err = np.mean((np.dot(x_test, w) - y_test) ** 2)
+        print "Test error: %s" % test_err
     
-    #degree = [2, 5, 10, 20]
+def ridge_reg(data):
+
+    degree_cases = [2, 5, 10, 20]
+    degree = 2
+    
+    x_train, y_train = data['X_trn'], data['Y_trn']
+    x_test, y_test = data['X_tst'], data['Y_tst'] 
+    x_train = poly_basis_trans(x_train, degree)
+    x_test = poly_basis_trans(x_test, degree)
+    
+    fold_cases = [2, 5, 10, x_train.shape[0]]
+    fold = 2
+    
+    np.random.shuffle(x_train, fold)
+    fold_size = int(x_train.shape[0]/fold)
+    x_train_test, x_train_train = x_train[:fold_size,:], x_train[fold_size:,:]
+    
+    #for lam in np.arange(0.001, 1000):
+    #for lam in [10 ** i for i in range(-3, 4)]:
+        
+    lam = 0.01
+    
+        
+
+    
+def cross_validation(data, fold):
+    np.random.shuffle(data)
+    
     
 if __name__ == '__main__':
-    #linear_reg()
-    X = np.array([[1,2], 
-                  [3,4]])
-    print X.shape
-    print range(2)
-    poly_basis_trans(X, 2)
+    data = scipy.io.loadmat('linear_regression.mat')
+
+#     X = np.array([[1,2], 
+#                   [3,4]])
+#     print X.shape
+#     print range(2)
+#     X = np.dot(X, X)
+#     print X
+    
+    #linear_reg(data)
+    #ridge_reg(data)
+    print range(-3, 3)
+    
